bf8891a3962742ab9e05e8ffdf1ddd7978a7d405
ProjectileAtlas <95831369+ProjectileAtlas@users.noreply.github.com>
2022-04-25 15:43:20 +0300

    Add files via upload

--- Pomodorotimer-main/script.js
+++ Pomodorotimer-main/script.js
@@ -8,19 +8,27 @@ let timing;
 let stop = document.getElementById("stop");
 let audio = new Audio();
 let mousebtn = new Audio();
+let halfing = 1000;
+let checker;
 audio.src = "get_over_here_sound_effects_-8776597565384612002.mp3";
 mousebtn.src = "Plate Click (Minecraft Sound) - Sound Effect for editing.m4a";
 stats.addEventListener("click", function () {
+  if (time == 3599) {
+    time = startingMinutes * 60;
+    checker = true;
+  }
   if (timing === undefined) {
-    timing = setInterval(updateCountdown, 1000);
+    timing = setInterval(updateCountdown, halfing);
     mousebtn.play();
-  } else {
+  } else if (checker == false) {
     alert("Timer is already running ");
   }
 });
+
 stop.addEventListener("click", function () {
   stopInterval();
   timing = undefined;
+
   mousebtn.play();
 });
 
@@ -29,17 +37,19 @@ function updateCountdown() {
   let seconds = time % 60;
   seconds = seconds < 10 ? "0" + seconds : seconds;
   countdownEl.innerHTML = `${minutes}:${seconds}`;
-  if (time > -2) {
+  if (time > -2 && time != 3599) {
     time--;
+    checker = false;
   }
-  console.log(time);
   if (time == -2) {
-    time = startingMinutes * 60;
+    checker = 25;
+    alert("You have completed a pomodoro, press start to restart.");
+
     audio.play();
-    alert("You have finished a pomodoro");
+
+    time = 3599;
   }
 }
-
 function stopInterval() {
   clearInterval(timing);
 }
